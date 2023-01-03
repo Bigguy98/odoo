c660d71fff30da5067300283acd56df34b5501d8
vuthanhtung98 <vuthanhtung98@vnpt.vn>
2023-01-03 09:50:45 +0700

    update entrypoint to init database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-python3 odoo-bin --addons-path=addons -r ${DB_USERNAME} -w ${DB_PASSWORD} --db_host ${DB_HOST} -d ${DB_NAME}
+python3 odoo-bin --addons-path=addons -i base -r ${DB_USERNAME} -w ${DB_PASSWORD} --db_host ${DB_HOST} -d ${DB_NAME}
